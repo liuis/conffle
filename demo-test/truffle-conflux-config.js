f4738b21be6248a341ca9e8ac09dfe783122c82f
liuis <liuisblockchain@gmail.com>
2019-09-25 19:22:23 +0800

    deploy code, hard code

--- demo-test/truffle-conflux-config.js
+++ demo-test/truffle-conflux-config.js
@@ -1,10 +1,16 @@
+
+import Cfx from 'conflux-web';
+
+
 module.exports = {
+  contracts_directory : "../demo-test/contracts",
+  contracts_build_directory: "../demo-test/contracts-output",
+  migrations_directory : "../demo-test/migrations",
   networks: {
     development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*", // Match any network id
-      gas: 5000000
+      host: "http://testnet-jsonrpc.conflux-chain.org",
+      port: 12537,
+      network_id: "*" // Match any network id
     }
   },
   compilers: {
