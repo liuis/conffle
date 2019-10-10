c7b764fb7890c06a9b0e717b5a42044297e438d7
liuis <liuisblockchain@gmail.com>
2019-10-10 11:12:14 +0800

    first publish

--- packages/conflux-chain-dapp-cli/conflux-dapp-deploy/deploy_contract.js
+++ packages/conflux-chain-dapp-cli/conflux-dapp-deploy/deploy_contract.js
@@ -1,22 +1,28 @@
-let source = fs.readFileSync("contracts.json");
-let contracts = JSON.parse(source)["contracts"];
+const fs = require('fs');
+const BN = require('bn.js');
+const ConfluxWeb = require('conflux-web');
 
-// ABI description as JSON structure
-let abi = JSON.parse(contracts.SampleContract.abi);
+const confluxWeb = new ConfluxWeb('http://testnet-jsonrpc.conflux-chain.org:12537');
 
-// Smart contract EVM bytecode as hex
-let code = contracts.SampleContract.bin;
+
+let source = fs.readFileSync("./demo-test/build/SampleContract.sol.json");
+
+const fd = require("./demo-test/build/SampleContract.sol.json");
+let abi = fd.abi
+let bytecode = fd.bytecode
+console.log("abi:", abi)
+console.log("bytecode:", bytecode)
+let code =  "0x" + bytecode
 
 // Create Contract proxy class
-let SampleContract = web3.cfx.contract(abi);
+let SampleContract = new confluxWeb.cfx.Contract(abi);
 
 console.log("Deploying the contract");
-let contract = SampleContract.new({from: web3.cfx.coinbase, gas: 1000000, data: code});
+let contract = SampleContract.deploy({from: 0, gas: 1000000, data: code});
 
 // Transaction has entered to geth memory pool
-console.log("Your contract is being deployed in transaction at http://testnet.etherscan.io/tx/" + contract.transactionHash);
+console.log("Your contract is being deployed in transaction at :" + contract.transactionHash);
 
-// http://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -25,13 +31,15 @@ function sleep(ms) {
 // in a block to get the address of the contract
 async function waitBlock() {
   while (true) {
-    let receipt = web3.cfx.getTransactionReceipt(contract.transactionHash);
+    let receipt = confluxWeb.cfx.getTransactionReceipt(contract.transactionHash);
     if (receipt && receipt.contractAddress) {
-      console.log("Your contract has been deployed at http://testnet.etherscan.io/address/" + receipt.contractAddress);
-      console.log("Note that it might take 30 - 90 sceonds for the block to propagate befor it's visible in etherscan.io");
+      console.log("Your contract has been deployed at :" + receipt.contractAddress);
+      console.log("Note that it might take some sceonds for the block to propagate befor it's visible in etherscan.io");
       break;
     }
-    console.log("Waiting a mined block to include your contract... currently in block " + web3.cfx.blockNumber);
+    EpochNumber = confluxWeb.cfx.getEpochNumber();
+    //Tx =confluxWeb.cfx.getBlockByEpochNumber(EpochNumber);
+    console.log("Waiting a mined block to include your contract... currently in epoch :" + EpochNumber.toString());
     await sleep(4000);
   }
 }
