05d16e05fa81954ae57224a0f236fe5098fcfc4a
liuis <ping.li@conflux.org>
2019-10-29 14:18:24 +0800

    add localhost node chain then deploy method

--- packages/conffle/deploy/deploy.js
+++ packages/conffle/deploy/deploy.js
@@ -1,9 +1,15 @@
 const BN = require('bn.js');
 const rlp = require('rlp');
 const keccak = require('keccak');
-
+var jayson = require('jayson');
+var client = jayson.client.http('http://localhost:12537');
 const ConfluxWeb = require('conflux-web');
-const confluxWeb = new ConfluxWeb('http://testnet-jsonrpc.conflux-chain.org:12537');
+const cfxNum = new BN('3000000000000000000');
+
+// use the testnet 
+//const confluxWeb = new ConfluxWeb('http://testnet-jsonrpc.conflux-chain.org:12537');
+// use the local node
+const confluxWeb = new ConfluxWeb('http://0.0.0.0:12537');
 
 const mnemonicInfo = require("conffle-utils/mnemonic");
 
@@ -15,21 +21,7 @@ async function run(address, privateKeys) {
 
     try {
         console.log("address:" + address, "privateKeys:" + privateKeys)
-        confluxWeb.cfx.accounts.wallet.add({
-            privateKey: privateKeys,
-            address: address
-        });
-
-        url = "http://testnet-jsonrpc.conflux-chain.org:18082/dev/ask?address=" + address
-        request(url, function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log("Has been sent to your account provided a CFX!!!")
-                console.log("Your address balance is :" + confluxWeb.cfx.getBalance(address));
-            }
-        });
-
-
-        await deployContract();
+        await deployContract(address, privateKeys);
 
     } catch (e) {
         console.error(e);
@@ -37,15 +29,6 @@ async function run(address, privateKeys) {
 }
 
 
-/*
-const {
-    mnemonic,
-    accounts,
-    privateKeys
-} = mnemonicInfo.getAccountsInfo(
-    1
-);
-*/
 function generate_contract_address(nonce, sender) {
 
     var input_arr = [sender, nonce];
@@ -90,7 +73,16 @@ function deploy(argument, abi) {
         }).catch(console.error);
 }
 
-function deployContract() {
+function deployContract(address, privateKeys) {
+
+        await sendBalance_localhost(address);
+
+        confluxWeb.cfx.accounts.wallet.add({
+            privateKey: privateKeys,
+            address: address
+        });
+
+
     fs.readdir("./demo-test/build", (err, files) => {
         files.forEach(file => {
             console.log(file);
@@ -103,7 +95,7 @@ function deployContract() {
             console.log("confluxWeb.cfx.accounts.wallet[0].address : ", confluxWeb.cfx.accounts.wallet[0].address);
             console.log("confluxWeb.cfx.accounts.wallet : ", confluxWeb.cfx.accounts.wallet);
             confluxWeb.cfx.getTransactionCount(confluxWeb.cfx.accounts.wallet[0].address).then(nonceValue => {
-            console.log("nonceValue:", nonceValue)
+                console.log("nonceValue:", nonceValue)
                 const txParams = {
                     from: 0,
                     nonce: nonceValue, // make nonce appropriate
@@ -142,6 +134,95 @@ function waitBlock(txHash, abi) {
         })
 
 }
+
+async function sendBalance_testnet(address) {
+
+
+    url = "http://testnet-jsonrpc.conflux-chain.org:18082/dev/ask?address=" + address
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log("Has been sent to your account a CFX coin!!!")
+            confluxWeb.cfx.getBalance(address).then((res) => console.log("Your address balance is :" + String(res)));
+
+        }
+    });
+
+
+}
+
+
+async function sendBalance_localhost(account) {
+    const GENESIS_PRI_KEY = "46b9e861b63d3509c88b7817275a30d22d62c8cd8fa6486ddee35ef0d8e0495f";
+    const GENESIS_ADDRESS = "0xfbe45681ac6c53d5a40475f7526bac1fe7590fb8";
+    const TO_ACCOUNT = account;
+
+    confluxWeb.cfx.accounts.wallet.add({
+        privateKey: GENESIS_PRI_KEY,
+        address: GENESIS_ADDRESS
+    })
+    confluxWeb.cfx.getTransactionCount(GENESIS_ADDRESS).then(async(nonceValue) => {
+        console.log("nonceValue:" + nonceValue)
+        let gasPrice = await confluxWeb.cfx.getGasPrice();
+        console.log("gasPrice : " + gasPrice)
+        let txParms = {
+            from: GENESIS_ADDRESS,
+            nonce: nonceValue,
+            gasPrice: 100,
+            data: "0x00",
+            value: cfxNum,
+            to: TO_ACCOUNT
+        };
+        let gas = await confluxWeb.cfx.estimateGas(txParms);
+        console.log("gas : ", gas)
+        txParms.gas = gas;
+        txParms.from = 0;
+        confluxWeb.cfx.signTransaction(txParms)
+            .then((encodedTransaction) => {
+                const {
+                    rawTransaction
+                } = encodedTransaction;
+                console.log('raw transaction: ', rawTransaction);
+                return confluxWeb.cfx.sendSignedTransaction(rawTransaction).then((transactionHash) => {
+                    console.log('transaction hash from RPC: ', transactionHash);
+                    //hexNonce = argument.nonce.toString(16);
+                    //contractAdd = generate_contract_address(hexNonce +1, confluxWeb.cfx.accounts.wallet[0].address);
+                    //console.log("Waiting a mined block to include your contract... contract address will be at:" + "0x" + contractAdd);
+
+                    localhost_waitBlock(transactionHash)
+                })
+            }).catch(console.error);
+    });
+}
+
+function localhost_waitBlock(txHash) {
+
+    for (var i = 0, len = 12; i < len; i++) {
+        client.request('generateoneblock', [10, 300000], function(err, error, result) {
+            if (err) throw err;
+            console.log("generateoneblock : " + result);
+        });
+
+    }
+
+
+    return confluxWeb.cfx.getTransactionReceipt(txHash).then(
+        (receipt) => {
+            console.log("Note that it might take some sceonds for the block to propagate befor it's visible in conflux");
+            if (receipt !== null) {
+                console.log("receipt:", receipt);
+                console.log("Your account has been receiver some cfx coin");
+                confluxWeb.cfx.getBalance(TO_ACCOUNT).then(console.log)
+                confluxWeb.cfx.wallet.remove(GENESIS_ADDRESS)
+            } else {
+                return localhost_waitBlock(txHash)
+            }
+        })
+}
+
+
+
+
+
 
 module.exports = {
     run
