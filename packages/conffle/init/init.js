566f6375d7cb89cbad36586ddc779b8b18e6a5c0
liuis <ping.li@conflux.org>
2019-11-04 14:20:29 +0800

    fxied the mkdir features

--- packages/conffle/init/init.js
+++ packages/conffle/init/init.js
@@ -1,23 +1,55 @@
 const TruffleBox = require("truffle-conflux-box");
-const mkdirp = require('mkdirp');
+const fs = require("fs");
+const path = require("path");
 const unboxOptions = {
     force: false
 };
 
-async function run() {
-    try {
-        await createProjectStructure();
 
-    } catch (e) {
-        console.error(e);
+function mkdirs(dirname, callback) {
+    fs.exists(dirname, function(exists) {
+        if (exists) {
+            callback();
+        } else {
+            //console.log(path.dirname(dirname));
+            mkdirs(path.dirname(dirname), function() {
+                fs.mkdir(dirname, callback);
+            });
+        }
+    });
+}
+
+function mkdirsSync(dirname) {
+    //console.log(dirname);  
+    if (fs.existsSync(dirname)) {
+        return true;
+    } else {
+        if (mkdirsSync(path.dirname(dirname))) {
+            fs.mkdirSync(dirname);
+            return true;
+        }
     }
 }
 
-function createProjectStructure(dir = "./demo-test") {
-    mkdirp('./demo-test/build', function(err) { 
-        console.log(err);
+async function run() {
+
+    mkdirs("./demo-test/build", function(err) {
+        if (err) console.error(err)
+        else {
+            try {
+
+                createProjectStructure();
+
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
     });
 
+}
+
+function createProjectStructure(dir = "./demo-test") {
     console.log("start the example procject, pls wait....")
 
     TruffleBox.unbox("https://github.com/liuis/truffle-conflux-init-default", dir, unboxOptions);
