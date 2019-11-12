3b9de4409a56fed0dd72706705e08d0ab8308c51
liuis <ping.li@conflux.org>
2019-11-12 18:08:29 +0800

    add contract op code , deployed/at/new etc..

---
+++ packages/conffle-interface-adapter/dist/Config.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const typescript_logging_1 = require("typescript-logging");
+// Optionally change default settings, in this example set default logging to Info.
+// Without changing configuration, categories will log to Error.
+typescript_logging_1.CategoryServiceFactory.setDefaultConfiguration(new typescript_logging_1.CategoryConfiguration(typescript_logging_1.LogLevel.Info));
+// Create categories, they will autoregister themselves, one category without parent (root) and a child category.
+exports.catService = new typescript_logging_1.Category("service");
+exports.catProd = new typescript_logging_1.Category("product", exports.catService);
+// Optionally get a logger for a category, since 0.5.0 this is not necessary anymore, you can use the category itself to log.
+// export const log: CategoryLogger = CategoryServiceFactory.getLogger(cat);
+//# sourceMappingURL=config.js.map
