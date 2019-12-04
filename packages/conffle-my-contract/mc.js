2ea0cf552115d6b4c3c5288fbebd730a388723bb
liuis <ping.li@conflux.org>
2019-12-04 14:00:55 +0800

    modify the test file

--- packages/conffle-my-contract/mc.js
+++ packages/conffle-my-contract/mc.js
@@ -1,4 +1,3 @@
-// Require the package that was previosly saved by truffle-artifactor
 const ConfluxWeb = require('conflux-web');
 var provider = new ConfluxWeb.providers.HttpProvider("http://0.0.0.0:12537");
 var contractTr = require("./index.js");
@@ -33,124 +32,19 @@ if (typeof MetaCoin.currentProvider.sendAsync !== "function") {
 var contract_address = "0x3a69dd57facd0e1751b85182b225a3b74ae7f0e3"
 var coin;
 
-//console.log(MetaCoin);
-console.log("1--------------------------------")
-console.log("2--------------------------------")
-console.log("3--------------------------------")
-    //coin.constructor.web3.cfx.accounts.wallet.add({
-    //    privateKey: pk,
-    //    address: ad
-    //});
+//coin.constructor.web3.cfx.accounts.wallet.add({
+//    privateKey: pk,
+//    address: ad
+//});
 MetaCoin.at(contract_address).then(function(instance) {
-        coin = instance;
+    coin = instance;
 
-        //console.log(util.inspect(coin.getBalance("0xe1680683be13895b59c94eaf61818975a0d105dd"), {
-        //    showHidden: true,
-        //    depth: 7
-        //}));
-        console.log(coin.getBalance("0xe1680683be13895b59c94eaf61818975a0d105dd"));
-        console.log(coin.getBalance(account_two));
-        //.then(result => {
-
-        //    console.log(result);
-
-        //})
-        /*
-        console.log(util.inspect(coin.sendCoin, {
-            showHidden: true,
-            depth: 7 
-        }));
-
-        */
-        console.log("--------------------------------")
-        //console.log(coin.sendCoin(account_two, 3))
+    //console.log(util.inspect(coin.getBalance("0xe1680683be13895b59c94eaf61818975a0d105dd"), {
+    //    showHidden: true,
+    //    depth: 7
+    //}));
+    console.log(coin.getBalance("0xe1680683be13895b59c94eaf61818975a0d105dd"));
+    console.log(coin.sendCoin(account_two, 3))
+    console.log(coin.getBalance(account_two));
+    console.log("--------------------------------")
 })
-
-
-/*
-        console.log(util.inspect(coin.sendCoin, {
-            showHidden: false,
-            depth: null
-        }));
-        console.log("--------------------------------")
-        return coin.sendCoin(account_two, 3, {
-            from: account_one
-        }).call();
-    }).then(function(result) {
-        console.log("result::::::::::", result);
-    })
-    //console.log("xxxx:", coin.constructor.web3.cfx.accounts.wallet);
-    //coin.getBalance(ad).then(result => {
-    //        console.log("result:::::::::", result)
-    //})
-    //console.log(coin.getBalance(ad))
-    ///console.log(util.inspect(coin.getBalance("0xe1680683be13895b59c94eaf61818975a0d105dd"), {
-    ///    showHidden: false,
-    ///    depth: null
-    ///}));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-    .then(function(result) {
-  // This code block will not be executed until truffle-contract has verified
-  // the transaction has been processed and it is included in a mined block.
-  // truffle-contract will error if the transaction hasn't been processed in 120 seconds.
-    console.log("result_@at:" + result);
-  // Since we're using promises, we can return a promise for a call that will
-  // check account two's balance.
-  return coin.methods.balances.call(account_two);
-}).then(function(balance_of_account_two) {
-  console.log("Balance of account two is " + balance_of_account_two + "!"); // => 3
-
-  // But maybe too much was sent. Let's send some back.
-  // Like before, will create a transaction that returns a promise, where
-  // the callback won't be executed until the transaction has been processed.
-  return coin.sendCoin(account_one, 1.5, {from: account_two});
-}).then(function(result) {
-  // Again, get the balance of account two
-  return coin.balances.call(account_two)
-}).then(function(balance_of_account_two) {
-  console.log("Balance of account two is " + balance_of_account_two + "!") // => 1.5
-}).catch(function(err) {
-  // Easily catch all errors along the whole execution.
-  console.log("ERROR! " + err.message);
-});
-
-////var deployed;
-////
-//console.log(util.inspect(coin, {showHidden: false, depth: null}));
-//})
-
-MetaCoin.new().then(function(instance) {
-  deployed = instance;
-  console.log(util.inspect(deployed, {showHidden: false, depth: null}));
-})
-MetaCoin.deployed().then(function(instance) {
-  deployed = instance;
-  console.log(util.inspect(deployed, {showHidden: false, depth: null}));
-})
-console.log("5--------------------------------")
-//console.log(util.inspect(coin, {showHidden: false, depth: null}));
-console.log("6--------------------------------")
-
-*/
